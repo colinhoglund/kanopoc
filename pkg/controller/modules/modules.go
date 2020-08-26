4d3939057cdf9cd3f7bb0b2aec969b29ffdc2065
Colin Hoglund <colinhoglund@gmail.com>
2020-08-26 12:35:36 -0700

    simplify module controller

--- pkg/controller/modules/modules.go
+++ pkg/controller/modules/modules.go
@@ -1,7 +1,6 @@
 package modules
 
 import (
-	"errors"
 	"kanopoc/pkg/config"
 	"kanopoc/pkg/module/drone"
 	"kanopoc/pkg/module/traefik"
@@ -23,38 +22,22 @@ func (c *Controller) Dump() {
 
 func (c *Controller) Apply() error {
 
-	moduleMap := map[string]interface{}{
+	moduleMap := map[string]Releaser{
 		"traefik": traefik.New(),
 		"drone":   drone.New(),
 	}
 
-	releasers, err := c.configureReleasers(moduleMap)
-	if err != nil {
-		return err
+	// configure modules
+	for path, mod := range moduleMap {
+		if err := c.config.Get(path).Scan(mod); err != nil {
+			return err
+		}
 	}
 
-	for _, mod := range releasers {
+	// apply modules
+	for _, mod := range moduleMap {
 		c.client.Apply(mod.ReleaseName(), mod.Chart())
 	}
 
 	return nil
-}
-
-func (c *Controller) configureReleasers(moduleMap map[string]interface{}) ([]Releaser, error) {
-	var releasers []Releaser
-
-	for path, mod := range moduleMap {
-		if err := c.config.Get(path).Scan(mod); err != nil {
-			return nil, err
-		}
-
-		r, ok := mod.(Releaser)
-		if !ok {
-			return nil, errors.New("Releaser type assertion error")
-		}
-
-		releasers = append(releasers, r)
-	}
-
-	return releasers, nil
 }
